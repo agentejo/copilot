553dcf104892d0fd6418d06fafe3e6720575fc73
Artur <artur@agentejo.com>
2015-11-04 00:45:57 +0100

    favicon for the backend added

--- cockpit/modules/core/Cockpit/views/layouts/app.php
+++ cockpit/modules/core/Cockpit/views/layouts/app.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $app['app.name'] }}</title>
+    <link rel="icon" href="@base('/favicon.ico')" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
     <script>
--- cockpit/modules/core/Cockpit/views/layouts/login.php
+++ cockpit/modules/core/Cockpit/views/layouts/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@lang('Authenticate Please!')</title>
+    <link rel="icon" href="@base('/favicon.ico')" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
     {{ $app->assets($app['app.assets.base'], $app['cockpit/version']) }}
